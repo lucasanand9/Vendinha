39da6825718787a3075a110fd64154fa74744b15
lucasanand9 <lucasanand9@gmail.com>
2023-04-08 15:47:02 -0300

    Adicionando os vetores

--- main.c
+++ main.c
@@ -10,6 +10,9 @@ int main(){
 if (n == 1 || n == 2){
 
     switch (n){
+      char vetorP[5][20] = {"Cafe Santa Monica", "Red Bull", "Pipoca Yoki", "Creme de leite", "Laranja"};
+      float valor[5];
+      float vetorQ[5];
         case 1:
             printf("Computador\n");
         break;
@@ -19,7 +22,7 @@ if (n == 1 || n == 2){
             while (w != 2){
                 printf("\nQuer comprar mais algum produto?\n");
                 printf("(1) Sim\n");
-                printf("(2) Nao\n");
+                printf("(2) Nao, finalizar compras\n");
                 scanf("%i", &w);
 
                 if (w == 1)
@@ -29,6 +32,7 @@ if (n == 1 || n == 2){
                     float kg;
                     float valorP;
                     float ValorKg;
+                    
                     
                     printf("\nQual dos produtos abaixo voce quer comprar?\n");
                     printf("(1) Café santa Monica\n");
@@ -43,8 +47,9 @@ if (n == 1 || n == 2){
                         printf("Café\n");
                         printf("Digite a quantidade desejada: \n");
                         scanf("%i", &q);
+                        vetorQ[0] = q;
                         valorP = q  * 57.04;
-                        printf("\nO valor é %.2f$", valorP);
+                        valor[0] = valorP;
 
                     break;
                     
@@ -52,32 +57,36 @@ if (n == 1 || n == 2){
                         printf("red bull\n");
                         printf("Digite a quantidade desejada: \n");
                         scanf("%i", &q);
+                        vetorQ[1] = q;
                         valorP = q * 193.90;
-                        printf("\nO valor é %.2f$", valorP);
+                        valor[1] = valorP;
                     break;
 
                     case 3:
                         printf("pipoca\n");
                         printf("Digite a quantidade desejada: \n");
                         scanf("%i", &q);
+                        vetorQ[2] = q;
                         valorP = q * 1.88;
-                        printf("\nO valor é %.2f$", valorP);
+                        valor[2] = valorP;
                     break;
 
                     case 4:
                         printf("creme de leite\n");
                         printf("Digite a quantidade desejada: \n");
                         scanf("%i", &q);
+                        vetorQ[3] = q;
                         valorP = q * 6.57;
-                        printf("\nO valor é %.2f$", valorP);
+                        valor[3] = valorP;
                     break;
 
                     case 5:
                         printf("laranja\n");
                         printf("Digite quantos kilos voce vai comprar: \n");
                         scanf("%f", &kg);
+                        vetorQ[4] = kg;
                         ValorKg = kg * 8.15;
-                        printf("\nO valor é %.2f$", ValorKg);
+                        valor[4] = ValorKg;
                     break;
 
                     default:
@@ -88,8 +97,25 @@ if (n == 1 || n == 2){
                 }
                 
             }
-                printf("\nTenha um bom dia! \nVolte sempre!\n");
-            break;
+            //mostrar o total
+            printf("Alimentos e Bebidas compradas:\n");
+            for (int i = 0; i < 5; i++){
+                //verificar o porque as strings nao estao sendo imprimidas
+                printf("%i: %s - %.1f Quantidades\n", i+1, vetorP[i], vetorQ[i]);
+                printf("O valor eh: %.2f Reais\n",valor[i]);
+            }
+
+            float valorT = 0;
+            for(int i; i < 5; i++){
+                valorT = valorT + valor[i];
+            }
+    
+                printf("\nO Frete: gratis");
+                printf("\nValor total a ser pago é %.2f Reais.", valorT);
+                printf("\nEndereco de entrega:"); 
+                //Aqui tenho que colocar o endereço(ultilizando vetores)
+                
+        break;
 
     default:
         break;
